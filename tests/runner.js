a611ebe4e05f5a67e043765ea3dc6591b8e333c7
Samori Gorse <devnull@localhost>
2011-01-30 23:23:58 +0100

    - Tests were previously runned using a setTimeout ( ?? ) This commit removes the hack and provides a clearer way to load the scripts.
    I know use HTMLScriptElement.onload or HTMLScriptElement.onreadystatechange
    to determine when all the scripts are loaded. When this happen, we call
    a callback.
    
    - Updated the TODO list

--- tests/runner.js
+++ tests/runner.js
@@ -1,35 +1,33 @@
 var assert = {
     log : function() {
-	//FIX ME
-	    return typeof(console) != 'undefined' ? console.log : print;
+    //FIX ME
+        return typeof(console) != 'undefined' ? console.log : print;
     }(),
     tests : 0,
     failed : 0,
     equals : function(given, expected) {
-	    this.tests += 1;
-	    if(given != expected) {
-	        this.failed += 1;
-	        this.log('Failed: "' + given + '" does not equal "' + expected + '"');
-	    }
+        this.tests += 1;
+        if(given != expected) {
+            this.failed += 1;
+            this.log('Failed: "' + given + '" does not equal "' + expected + '"');
+        }
     },
     summary : function() {
-	    this.log();
-	    this.log(this.tests + ' tests, ' + this.failed + ' failed');
+        this.log(this.tests + ' tests, ' + this.failed + ' failed');
     }
 };
 
-load(
+load([
     './decimaljs.js',
-    'tests/addition.js',
     'tests/decimal_type.js',
     'tests/format.js',
-    'tests/multiplication.js',
     'tests/neg_exp.js',
     'tests/pos_exp.js',
     'tests/repr.js',
-    'tests/subtraction.js'
-);
-
-setTimeout(function() {
-    assert.summary()
-}, 2000);
+    'tests/addition.js',
+    'tests/subtraction.js',
+    'tests/multiplication.js',
+    'tests/division.js'
+], function() {
+    assert.summary();
+});
