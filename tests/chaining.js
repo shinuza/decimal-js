578d5c1f8cac4e316e4ec299e335c1e68ae5b48e
Samori Gorse <samori@codeinstyle.io>
2025-01-03 12:45:08 +0100

    feat: Modernize tests (#12)
    - Modernize test files
    - Updated package.json to support modules

--- tests/chaining.js
+++ tests/chaining.js
@@ -1,6 +1,10 @@
-var Decimal = Decimal || require('../lib/decimal.js');
-var assert = assert || require('assert');
+import Decimal from '../lib/decimal.js';
+
+import assert from 'assert';
+import test from 'node:test';
 
 
-assert.equal(Decimal(1.005).mul(50.01).mul(0.03), "1.5078015");
-assert.equal(Decimal(1.005).div(50.01).add(0.03), "0.05009598080383923");
+test('chaining', function(t) {
+    assert.equal(Decimal(1.005).mul(50.01).mul(0.03), "1.5078015");
+    assert.equal(Decimal(1.005).div(50.01).add(0.03), "0.05009598080383923");
+});
